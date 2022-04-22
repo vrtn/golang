458c2318008f7d64402fa3ef8a7808f51b3356c8
Zanini <everton.zanini@yahoo.com>
2022-04-22 14:51:32 -0300

    EX0101, concluído

---
+++ Ex0101/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Print(x, "\n")
+	fmt.Print(y, "\n")
+	fmt.Print(z)
+}
